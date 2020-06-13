11bec4aa256a415283b440087348443218dccb7f
Sebastian Czech <sebaczech@gmail.com>
2020-06-14 00:39:00 +0200

    feature: Send sander if temp. below 0

--- src/main/java/snow/SnowRescueService.java
+++ src/main/java/snow/SnowRescueService.java
@@ -6,10 +6,20 @@ import snow.dependencies.WeatherForecastService;
 
 public class SnowRescueService {
 
+	private final WeatherForecastService weatherForecastService;
+	private final MunicipalServices municipalServices;
+	private final PressService pressService;
+
 	public SnowRescueService(WeatherForecastService weatherForecastService, MunicipalServices municipalServices, PressService pressService) {
+		this.weatherForecastService = weatherForecastService;
+		this.municipalServices = municipalServices;
+		this.pressService = pressService;
 	}
 
-	public void checkForecastAndRescue() {
+	void checkForecastAndRescue() {
+		if (weatherForecastService.getAverageTemperatureInCelsius() < 0) {
+			municipalServices.sendSander();
+		}
 	}
 
 }
--- src/test/java/snow/SnowRescueServiceTest.java
+++ src/test/java/snow/SnowRescueServiceTest.java
@@ -1,4 +1,25 @@
 package snow;
 
-public class SnowRescueServiceTest {
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import snow.dependencies.MunicipalServices;
+import snow.dependencies.WeatherForecastService;
+
+class SnowRescueServiceTest {
+
+    @Test
+    void send_sander_when_temperature_belowe_zero() {
+        // given
+        WeatherForecastService weatherForecastService = Mockito.mock(WeatherForecastService.class);
+        MunicipalServices municipalServices = Mockito.mock(MunicipalServices.class);
+        Mockito.when(weatherForecastService.getAverageTemperatureInCelsius()).thenReturn(-1);
+        SnowRescueService snowRescueService = new SnowRescueService(weatherForecastService, municipalServices, null);
+
+        // when
+        snowRescueService.checkForecastAndRescue();
+
+        // then
+        Mockito.verify(municipalServices).sendSander();
+    }
+
 }
