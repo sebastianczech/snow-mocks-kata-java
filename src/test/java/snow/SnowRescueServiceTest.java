3ddc41020ee6d106cf1e7a81d250245889383c7a
Sebastian Czech <sebaczech@gmail.com>
2020-06-14 01:00:03 +0200

    refactor: Use annotation instead of static method

--- src/test/java/snow/SnowRescueServiceTest.java
+++ src/test/java/snow/SnowRescueServiceTest.java
@@ -1,19 +1,35 @@
 package snow;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import snow.dependencies.MunicipalServices;
 import snow.dependencies.WeatherForecastService;
 
+@ExtendWith(MockitoExtension.class)
 class SnowRescueServiceTest {
 
-    @Test
-    void send_sander_when_temperature_belowe_zero() {
-        // given
-        WeatherForecastService weatherForecastService = Mockito.mock(WeatherForecastService.class);
-        MunicipalServices municipalServices = Mockito.mock(MunicipalServices.class);
+    @Mock WeatherForecastService weatherForecastService;
+    @Mock MunicipalServices municipalServices;
+
+    private SnowRescueService snowRescueService;
+
+    @BeforeEach
+    void setup() {
         Mockito.when(weatherForecastService.getAverageTemperatureInCelsius()).thenReturn(-1);
-        SnowRescueService snowRescueService = new SnowRescueService(weatherForecastService, municipalServices, null);
+        snowRescueService = new SnowRescueService(weatherForecastService, municipalServices, null);
+    }
+
+    @Test
+    void send_sander_when_temperature_below_zero() {
+        // given
+//        WeatherForecastService weatherForecastService = Mockito.mock(WeatherForecastService.class);
+//        MunicipalServices municipalServices = Mockito.mock(MunicipalServices.class);
+//        Mockito.when(weatherForecastService.getAverageTemperatureInCelsius()).thenReturn(-1);
+//        SnowRescueService snowRescueService = new SnowRescueService(weatherForecastService, municipalServices, null);
 
         // when
         snowRescueService.checkForecastAndRescue();
